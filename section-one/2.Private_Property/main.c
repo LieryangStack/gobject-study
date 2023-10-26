d8b13965955f006413d3d51ab253162ba2d73f9d
LieryangStack <SXlisibo@gmail.com>
2023-10-26 20:19:33 +0800

    update

--- section-one/2.Private_Property/main.c
+++ section-one/2.Private_Property/main.c
@@ -1,16 +1,12 @@
 #include "pm-dlist.h"
   
 int
-main (void)
-{
-        /* GObject 库的类型管理系统的初始化 */
-        g_type_init ();
-     
+main (void) {
         PMDList *list;
- 
+
         list = g_object_new (PM_TYPE_DLIST, NULL);
 
         g_object_unref (list);
-          
+        
         return 0;
 }
